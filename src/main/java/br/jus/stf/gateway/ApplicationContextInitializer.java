8aea8032418e8ece42030b6c5b297ea6eff2eeec
Rodrigo Barreiros <barreiros.rodrigo@gmail.com>
2016-07-02 15:52:22 -0300

    Habilitando segurança no Gateway, que agora é um componente do SSO com Oauth2

--- src/main/java/br/jus/stf/gateway/ApplicationContextInitializer.java
+++ src/main/java/br/jus/stf/gateway/ApplicationContextInitializer.java
@@ -2,15 +2,14 @@ package br.jus.stf.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.undertow.UndertowDeploymentInfoCustomizer;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.boot.context.embedded.undertow.UndertowEmbeddedServletContainerFactory;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-
-import io.undertow.servlet.api.DeploymentInfo;
 
 /**
  * @author Rodrigo Barreiros
@@ -21,55 +20,60 @@ import io.undertow.servlet.api.DeploymentInfo;
 @SpringBootApplication
 @EnableZuulProxy
 public class ApplicationContextInitializer {
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(ApplicationContextInitializer.class, args);
 	}
-	
+
 	/**
-	 * Este permite customizar o Undertow para permitir o forwarding de erros na resposta de requisição
-	 * feita ao gateway
+	 * Este permite customizar o Undertow para permitir o forwarding de erros na
+	 * resposta de requisição feita ao gateway
 	 */
 	@Bean
-    public UndertowEmbeddedServletContainerFactory embeddedServletContainerFactory() {
-        UndertowEmbeddedServletContainerFactory factory = new UndertowEmbeddedServletContainerFactory();
-        factory.addDeploymentInfoCustomizers(new UndertowDeploymentInfoCustomizer() {
-            @Override
-            public void customize(DeploymentInfo deploymentInfo) {
-                deploymentInfo.setAllowNonStandardWrappers(true);
-            }
-        });
-        return factory;
-    }
-	
-	/**
-	 * A <a href="http://www.w3.org/Security/wiki/Same_Origin_Policy">Política de Mesma Origem</a>
-	 * é um importante conceito de segurança implementado pelos navegadores para evitar que códigos 
-	 * Javascript façam requições para um domínio diferente do seu domínio de origem. 
-	 * 
-	 * Cross-Origin Resource Sharing (CORS) é a técnica usada para flexibilizar a aplicação da
-	 * política para permitir que um código Javascript em uma página possa consumir a API REST 
-	 * de um outro domínio.
-	 */
-	@Bean
-	public CorsFilter corsFilter() {
-	    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-	    final CorsConfiguration config = new CorsConfiguration();
-	    /**
-	     * Todos as origens são permitidas.
-	     */
-	    config.addAllowedOrigin(CorsConfiguration.ALL);
-	    /**
-	     * Apenas os headers abaixo são suportados. Uma requisição com um header fora dessa lista será rejeitada.
-	     */
-	    config.addAllowedHeader("Content-Type");
-	    config.addAllowedHeader("Accept");
-	    /**
-	     * Todos os métodos abaixo são suportados. Uma requisição com método fora dessa lista será rejeitada.
-	     */
-	    config.addAllowedMethod(CorsConfiguration.ALL);
-	    source.registerCorsConfiguration("/**", config);
-	    return new CorsFilter(source);
+	public UndertowEmbeddedServletContainerFactory embeddedServletContainerFactory() {
+		UndertowEmbeddedServletContainerFactory factory = new UndertowEmbeddedServletContainerFactory();
+		factory.addDeploymentInfoCustomizers(di -> di.setAllowNonStandardWrappers(true));
+		return factory;
 	}
-	
+
+	/**
+	 * A <a href="http://www.w3.org/Security/wiki/Same_Origin_Policy">Política
+	 * de Mesma Origem</a> é um importante conceito de segurança implementado
+	 * pelos navegadores para evitar que códigos Javascript façam requições para
+	 * um domínio diferente do seu domínio de origem.
+	 * 
+	 * Cross-Origin Resource Sharing (CORS) é a técnica usada para flexibilizar
+	 * a aplicação da política para permitir que um código Javascript em uma
+	 * página possa consumir a API REST de um outro domínio.
+	 */
+	@Bean
+	public FilterRegistrationBean corsFilter() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		final CorsConfiguration config = new CorsConfiguration();
+		/**
+		 * Todos as origens são permitidas.
+		 */
+		config.addAllowedOrigin(CorsConfiguration.ALL);
+		/**
+		 * Apenas os headers abaixo são suportados. Uma requisição com um header
+		 * fora dessa lista será rejeitada.
+		 */
+		config.addAllowedHeader("Authorization");
+		config.addAllowedHeader("X-Requested-With");
+		config.addAllowedHeader("X-XSRF-TOKEN");
+		config.addAllowedHeader("Content-Type");
+		config.addAllowedHeader("Accept");
+		/**
+		 * Todos os métodos abaixo são suportados. Uma requisição com método
+		 * fora dessa lista será rejeitada.
+		 */
+		config.addAllowedMethod(CorsConfiguration.ALL);
+		source.registerCorsConfiguration("/**", config);
+		
+		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		
+		return bean;
+	}
+
 }
---
+++ src/main/java/br/jus/stf/gateway/configurer/WebSecurityConfigurer.java
@@ -0,0 +1,101 @@
+package br.jus.stf.gateway.configurer;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.provider.authentication.BearerTokenExtractor;
+import org.springframework.security.oauth2.provider.authentication.TokenExtractor;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.DefaultCsrfToken;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.WebUtils;
+
+/**
+ * @author Rodrigo Barreiros
+ * 
+ * @since 1.0.0
+ * @since 01.07.2016
+ */
+@EnableResourceServer
+@Configuration
+public class WebSecurityConfigurer extends ResourceServerConfigurerAdapter {
+	
+	private TokenExtractor tokenExtractor = new BearerTokenExtractor();
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http
+			.antMatcher("/**").authorizeRequests()
+				.antMatchers("/discovery/api/routes", "/discovery/api/queries", "/documents/api/onlyoffice/baseUrl", "/userauthentication/oauth/token", "/**/bundle.js*").permitAll()
+				.anyRequest().authenticated()
+			.and()
+				.csrf().ignoringAntMatchers("/discovery/api/routes", "/discovery/api/queries", "/documents/api/onlyoffice/baseUrl", "/userauthentication/oauth/token").csrfTokenRepository(new CustomHttpSessionCsrfTokenRepository())
+			.and()
+				.addFilterAfter(csrfHeaderFilter(), CsrfFilter.class);
+	}
+	
+	private Filter csrfHeaderFilter() {
+		return new OncePerRequestFilter() {
+			@Override
+			protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+				CsrfToken csrf = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
+				if (csrf != null) {
+					Cookie cookie = WebUtils.getCookie(request, "XSRF-TOKEN");
+					String token = csrf.getToken();
+					if (cookie == null || token != null && !token.equals(cookie.getValue())) {
+						cookie = new Cookie("XSRF-TOKEN", token);
+						cookie.setPath("/");
+						response.addCookie(cookie);
+					}
+				}
+				filterChain.doFilter(request, response);
+			}
+		};
+	}
+
+	class CustomHttpSessionCsrfTokenRepository implements CsrfTokenRepository {
+		
+		private HttpSessionCsrfTokenRepository wrapped = new HttpSessionCsrfTokenRepository();
+		
+		public CustomHttpSessionCsrfTokenRepository() {
+			wrapped.setHeaderName("X-XSRF-TOKEN");
+		}
+
+		@Override
+		public CsrfToken generateToken(HttpServletRequest request) {
+			CsrfToken token = wrapped.generateToken(request);
+			
+			Authentication authToken = tokenExtractor.extract(request);
+			
+			if (authToken != null) {
+				return new DefaultCsrfToken(token.getHeaderName(), token.getParameterName(), authToken.getPrincipal().toString());
+			}
+			
+			return token;
+		}
+
+		@Override
+		public void saveToken(CsrfToken token, HttpServletRequest request, HttpServletResponse response) {
+			wrapped.saveToken(token, request, response);
+		}
+
+		@Override
+		public CsrfToken loadToken(HttpServletRequest request) {
+			return wrapped.loadToken(request);
+		}
+	}
+}
